87f0255912b9cd4cb149bf7748c797c0c2537b2d
liuxiaokun <1844861420@qq.com>
2021-06-02 16:17:33 +0800

    add redis module

---
+++ common-redis/src/main/java/com/lxk/project/common/redis/RedisConfig.java
@@ -0,0 +1,12 @@
+package com.lxk.project.common.redis;
+
+/**
+ * @Description TODO
+ * @Author liuxiaokun@e6yun.com
+ * @Created Date: 2021/5/27 11:08
+ * @ClassName RedisConfig
+ * @Remark
+ */
+
+public class RedisConfig {
+}
---
+++ common-redis/src/main/java/com/lxk/project/common/redis/RedisService.java
@@ -0,0 +1,12 @@
+package com.lxk.project.common.redis;
+
+/**
+ * @Description TODO
+ * @Author liuxiaokun@e6yun.com
+ * @Created Date: 2021/5/27 11:07
+ * @ClassName RedisService
+ * @Remark
+ */
+
+public class RedisService {
+}
